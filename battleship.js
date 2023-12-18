32d62bfdd40bfebc6683449b84699e19bedeb14e
HolidayGuy21 <14449@coderacademy.edu.au>
2023-12-18 15:12:36 +1100

    created tests for and functionality for placing ships at given coordinates (both vertically and horizontally)

--- battleship.js
+++ battleship.js
@@ -14,4 +14,39 @@ const ship = (length, hitsTaken, sunken) => ({
   }
 });
 
-module.exports = { ship }
+const gameboard = {
+  leBoard: [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ],
+  printBoard: function () {
+    console.log(this.leBoard);
+  },
+  receiveShip: function (coordX, coordY, placementDirection, ship) {
+if (placementDirection === 'vertical') {
+  let i = 0;
+  while (i < ship.length) {
+    this.leBoard[coordX][coordY] = 1;
+    coordX += 1;
+    i++;
+  }
+} else {
+  let i = 0;
+  while (i < ship.length) {
+    this.leBoard[coordX][coordY] = 1;
+    coordY += 1;
+    i++;
+      }
+    }
+  }
+};
+
+module.exports = { ship, gameboard }
--- battleship.test.js
+++ battleship.test.js
@@ -12,3 +12,23 @@ test("Ship function returns a Battleship object correctly", () => {
     battleship.isSunk()
     expect(battleship.sunken).toBeTruthy();
 })
+
+test("Gameboard is able to place ship vertically at given coordinates", () => {
+    const battleship = myModules.ship(4, 0, false);
+    myModules.gameboard.receiveShip(2, 3, 'vertical', battleship)
+    expect(myModules.gameboard.leBoard[2][3]).toBe(1)
+    expect(myModules.gameboard.leBoard[3][3]).toBe(1)
+    expect(myModules.gameboard.leBoard[4][3]).toBe(1);
+    expect(myModules.gameboard.leBoard[5][3]).toBe(1);
+    expect(myModules.gameboard.leBoard[6][3]).toBe(0);
+})
+
+test("Gameboard is able to place ship horizontally at given coordinates", () => {
+  const battleship = myModules.ship(4, 0, false);
+  myModules.gameboard.receiveShip(2, 3, null, battleship);
+  expect(myModules.gameboard.leBoard[2][3]).toBe(1);
+  expect(myModules.gameboard.leBoard[2][4]).toBe(1);
+  expect(myModules.gameboard.leBoard[2][5]).toBe(1);
+  expect(myModules.gameboard.leBoard[2][6]).toBe(1);
+  expect(myModules.gameboard.leBoard[2][7]).toBe(0);
+});
