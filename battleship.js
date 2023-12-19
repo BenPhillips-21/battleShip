62b6c4665792ff39c551dbb95906184c38f929b3
HolidayGuy21 <14449@coderacademy.edu.au>
2023-12-19 16:04:45 +1100

    fixed receive ship functionality, ships now can't be placed on occupied spaces and can be placed anywhere within the board - wrote tests for all

--- battleship.js
+++ battleship.js
@@ -28,10 +28,23 @@ const gameboard = {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ],
   printBoard: function () {
-    console.log(this.leBoard);
+    console.log(this.leBoard.map((row) => row.join(" ")).join("\n"));
   },
   receiveShip: function (coordX, coordY, placementDirection, ship) {
+if (coordX > 9 || coordY > 9) {
+  return "You can't place your ship there Admiral!";
+}
 if (placementDirection === 'vertical') {
+    if (coordX + (ship.length - 1) > 9) {
+      return "You can't place your ship there Admiral!";
+    }
+    let coordXcopy = coordX;
+    for (let i = 0; i < ship.length; i++) {
+      if (this.leBoard[coordXcopy][coordY]) {
+        return "A ship already occupies this space Admiral!"
+      }
+      coordXcopy++
+    }
   let i = 0;
   while (i < ship.length) {
     this.leBoard[coordX][coordY] = 1;
@@ -39,14 +52,35 @@ if (placementDirection === 'vertical') {
     i++;
   }
 } else {
-  let i = 0;
-  while (i < ship.length) {
+  if (coordY + (ship.length - 1) > 9) {
+    return "You can't place your ship there Admiral!";
+  }
+    let coordYcopy = coordY;
+    for (let i = 0; i < ship.length; i++) {
+      if (this.leBoard[coordX][coordYcopy]) {
+        return "A ship already occupies this space Admiral!";
+      }
+      coordYcopy++;
+    }
+  let t = 0;
+  while (t < ship.length) {
     this.leBoard[coordX][coordY] = 1;
     coordY += 1;
-    i++;
-      }
-    }
+    t++;
+  }
+}
   }
 };
+
+// const battleship = ship(4, 0, false);
+// let result = gameboard.receiveShip(3, 6, 'vertical', battleship)
+// console.log(result)
+// gameboard.printBoard();
+
+// const carrier = ship(5, 0, false);
+// let result1 = gameboard.receiveShip(4, 0, "horizontal", carrier);
+// console.log(result1);
+// gameboard.printBoard();
+
 
 module.exports = { ship, gameboard }
--- battleship.test.js
+++ battleship.test.js
@@ -1,5 +1,7 @@
 const myModules = require("./battleship.js")
 
+// leBoard[Y][X]
+
 test("Ship function returns a Battleship object correctly", () => {
     const battleship = myModules.ship(4, 0, false)
     expect(battleship.length).toBe(4)
@@ -13,22 +15,39 @@ test("Ship function returns a Battleship object correctly", () => {
     expect(battleship.sunken).toBeTruthy();
 })
 
-test("Gameboard is able to place ship vertically at given coordinates", () => {
+test("Gameboard is able to place ship horizontally at given coordinates", () => {
     const battleship = myModules.ship(4, 0, false);
-    myModules.gameboard.receiveShip(2, 3, 'vertical', battleship)
-    expect(myModules.gameboard.leBoard[2][3]).toBe(1)
-    expect(myModules.gameboard.leBoard[3][3]).toBe(1)
-    expect(myModules.gameboard.leBoard[4][3]).toBe(1);
-    expect(myModules.gameboard.leBoard[5][3]).toBe(1);
-    expect(myModules.gameboard.leBoard[6][3]).toBe(0);
+    myModules.gameboard.receiveShip(0, 0, "horizontal", battleship)
+    expect(myModules.gameboard.leBoard[0][0]).toBe(1)
+    expect(myModules.gameboard.leBoard[0][3]).toBe(1);
+    expect(myModules.gameboard.leBoard[0][5]).toBe(0);
 })
 
-test("Gameboard is able to place ship horizontally at given coordinates", () => {
-  const battleship = myModules.ship(4, 0, false);
-  myModules.gameboard.receiveShip(2, 3, null, battleship);
-  expect(myModules.gameboard.leBoard[2][3]).toBe(1);
-  expect(myModules.gameboard.leBoard[2][4]).toBe(1);
-  expect(myModules.gameboard.leBoard[2][5]).toBe(1);
-  expect(myModules.gameboard.leBoard[2][6]).toBe(1);
-  expect(myModules.gameboard.leBoard[2][7]).toBe(0);
+test("Gameboard is able to place ship vertically at given coordinates", () => {
+  const carrier = myModules.ship(5, 0, false);
+  myModules.gameboard.receiveShip(5, 9, 'vertical', carrier);
+  myModules.gameboard.printBoard();
+  expect(myModules.gameboard.leBoard[4][9]).toBe(0)
+  expect(myModules.gameboard.leBoard[5][9]).toBe(1);
+  expect(myModules.gameboard.leBoard[9][9]).toBe(1);
 });
+
+test("gameboard should not be able to place a ship vertically in a spot without sufficient space", () => {
+    const battleship = myModules.ship(4, 0, false);
+    let result = myModules.gameboard.receiveShip(8, 0, 'vertical', battleship);
+    expect(result).toBe("You can't place your ship there Admiral!")
+})
+
+
+test("gameboard should not be able to place a ship horizontally in a spot without sufficient space", () => {
+  const battleship = myModules.ship(4, 0, false);
+  let result = myModules.gameboard.receiveShip(10, 7, 'horizontal', battleship);
+  expect(result).toBe("You can't place your ship there Admiral!");
+});
+
+test("gameboard should not be able to place a ship in an occupied space", () => {
+    myModules.gameboard.printBoard();
+    const destroyer = myModules.ship(3, 0, false)
+    let result = myModules.gameboard.receiveShip(0, 0, 'horizontal', destroyer)
+    expect(result).toBe("A ship already occupies this space Admiral!");
+})
