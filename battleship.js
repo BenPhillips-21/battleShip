fdf4d0336f5c16b3eae87e7bbdb9c8461efac762
HolidayGuy21 <14449@coderacademy.edu.au>
2023-12-19 20:14:23 +1100

    added receiveAttack function, gameboard keeps track of missed shots and records them on the board, gameboard is able to report whether on all ships have been sunk with checkFleet function

--- battleship.js
+++ battleship.js
@@ -1,17 +1,19 @@
-const ship = (length, hitsTaken, sunken) => ({
+let ships = [];
+const ship = (length, hitsTaken, sunken, shipId) => ({
   length,
   hitsTaken,
   sunken,
+  shipId,
   hit: function () {
-    this.hitsTaken += 1
-    return this.hitsTaken
+    this.hitsTaken += 1;
+    return this.hitsTaken;
   },
   isSunk: function () {
     if (this.hitsTaken === this.length) {
-        this.sunken = true
-        return this.sunken
+      this.sunken = true;
+      return this.sunken;
     }
-  }
+  },
 });
 
 const gameboard = {
@@ -25,62 +27,91 @@ const gameboard = {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ],
   printBoard: function () {
     console.log(this.leBoard.map((row) => row.join(" ")).join("\n"));
   },
   receiveShip: function (coordX, coordY, placementDirection, ship) {
-if (coordX > 9 || coordY > 9) {
-  return "You can't place your ship there Admiral!";
-}
-if (placementDirection === 'vertical') {
-    if (coordX + (ship.length - 1) > 9) {
+    if (coordX > 9 || coordY > 9) {
       return "You can't place your ship there Admiral!";
     }
-    let coordXcopy = coordX;
-    for (let i = 0; i < ship.length; i++) {
-      if (this.leBoard[coordXcopy][coordY]) {
-        return "A ship already occupies this space Admiral!"
+    if (placementDirection === "vertical") {
+      if (coordX + (ship.length - 1) > 9) {
+        return "You can't place your ship there Admiral!";
       }
-      coordXcopy++
-    }
-  let i = 0;
-  while (i < ship.length) {
-    this.leBoard[coordX][coordY] = 1;
-    coordX += 1;
-    i++;
-  }
-} else {
-  if (coordY + (ship.length - 1) > 9) {
-    return "You can't place your ship there Admiral!";
-  }
-    let coordYcopy = coordY;
-    for (let i = 0; i < ship.length; i++) {
-      if (this.leBoard[coordX][coordYcopy]) {
-        return "A ship already occupies this space Admiral!";
+      let coordXcopy = coordX;
+      for (let i = 0; i < ship.length; i++) {
+        if (this.leBoard[coordXcopy][coordY]) {
+          return "A ship already occupies this space Admiral!";
+        }
+        coordXcopy++;
       }
-      coordYcopy++;
+      let i = 0;
+      while (i < ship.length) {
+        this.leBoard[coordX][coordY] = ship.shipId;
+        coordX += 1;
+        i++;
+      }
+      ships.push(ship)
+    } else {
+      if (coordY + (ship.length - 1) > 9) {
+        return "You can't place your ship there Admiral!";
+      }
+      let coordYcopy = coordY;
+      for (let i = 0; i < ship.length; i++) {
+        if (this.leBoard[coordX][coordYcopy]) {
+          return "A ship already occupies this space Admiral!";
+        }
+        coordYcopy++;
+      }
+      let t = 0;
+      while (t < ship.length) {
+        this.leBoard[coordX][coordY] = ship.shipId;
+        coordY += 1;
+        t++;
+      }
+      ships.push(ship);
     }
-  let t = 0;
-  while (t < ship.length) {
-    this.leBoard[coordX][coordY] = 1;
-    coordY += 1;
-    t++;
+  },
+checkFleet: function() {
+  let fleet = [];
+  for (var i = 0; i < ships.length; i++) {
+    fleet.push(ships[i].sunken);
   }
-}
+
+  if (fleet.every(ship => ship === true)) {
+    return 'The fleet has been destroyed!';
+  } else {
+    return 'The fleet is still in tact!'
   }
+},
+  receiveAttack: function (coordX, coordY) {
+    if (this.leBoard[coordX][coordY] > 0) {
+      for (var i = 0; i < ships.length; i++) {
+        if (this.leBoard[coordX][coordY] === ships[i].shipId) {
+          ships[i].hit();
+          ships[i].isSunk();
+          this.leBoard[coordX][coordY] = "X";
+          let result = gameboard.checkFleet();
+          console.log(result);
+          if (result === "The fleet has been destroyed!") {
+            return result
+          } else {
+            return "Direct hit!";
+          } 
+        }
+      }
+    } else {
+      this.leBoard[coordX][coordY] = "X";
+      return "Miss!";
+    }
+  },
 };
 
-// const battleship = ship(4, 0, false);
-// let result = gameboard.receiveShip(3, 6, 'vertical', battleship)
+// let x = ship(5, 0, false, 1)
+// gameboard.receiveShip(5, 5, 'vertical', x)
+// let result = gameboard.checkFleet()
 // console.log(result)
-// gameboard.printBoard();
 
-// const carrier = ship(5, 0, false);
-// let result1 = gameboard.receiveShip(4, 0, "horizontal", carrier);
-// console.log(result1);
-// gameboard.printBoard();
-
-
-module.exports = { ship, gameboard }
+module.exports = { ship, gameboard, ships };
--- battleship.test.js
+++ battleship.test.js
@@ -1,9 +1,9 @@
 const myModules = require("./battleship.js")
 
-// leBoard[Y][X]
+// Fix ships array. It should be enacted within receiveship, not separately
 
 test("Ship function returns a Battleship object correctly", () => {
-    const battleship = myModules.ship(4, 0, false)
+    const battleship = myModules.ship(4, 0, false, 1)
     expect(battleship.length).toBe(4)
     expect(battleship.hitsTaken).toBe(0)
     expect(battleship.sunken).toBeFalsy()
@@ -16,38 +16,61 @@ test("Ship function returns a Battleship object correctly", () => {
 })
 
 test("Gameboard is able to place ship horizontally at given coordinates", () => {
-    const battleship = myModules.ship(4, 0, false);
-    myModules.gameboard.receiveShip(0, 0, "horizontal", battleship)
-    expect(myModules.gameboard.leBoard[0][0]).toBe(1)
-    expect(myModules.gameboard.leBoard[0][3]).toBe(1);
+    const cruiser = myModules.ship(4, 0, false, 2);
+    myModules.gameboard.receiveShip(0, 0, "horizontal", cruiser)
+    expect(myModules.gameboard.leBoard[0][0]).toBe(cruiser.shipId)
+    expect(myModules.gameboard.leBoard[0][3]).toBe(cruiser.shipId);
     expect(myModules.gameboard.leBoard[0][5]).toBe(0);
 })
 
 test("Gameboard is able to place ship vertically at given coordinates", () => {
-  const carrier = myModules.ship(5, 0, false);
+  const carrier = myModules.ship(5, 0, false, 3);
   myModules.gameboard.receiveShip(5, 9, 'vertical', carrier);
-  myModules.gameboard.printBoard();
   expect(myModules.gameboard.leBoard[4][9]).toBe(0)
-  expect(myModules.gameboard.leBoard[5][9]).toBe(1);
-  expect(myModules.gameboard.leBoard[9][9]).toBe(1);
+  expect(myModules.gameboard.leBoard[5][9]).toBe(carrier.shipId);
+  expect(myModules.gameboard.leBoard[9][9]).toBe(carrier.shipId);
 });
 
 test("gameboard should not be able to place a ship vertically in a spot without sufficient space", () => {
-    const battleship = myModules.ship(4, 0, false);
-    let result = myModules.gameboard.receiveShip(8, 0, 'vertical', battleship);
+    const victory = myModules.ship(4, 0, false, 4);
+    let result = myModules.gameboard.receiveShip(8, 0, 'vertical', victory);
     expect(result).toBe("You can't place your ship there Admiral!")
 })
 
 
 test("gameboard should not be able to place a ship horizontally in a spot without sufficient space", () => {
-  const battleship = myModules.ship(4, 0, false);
-  let result = myModules.gameboard.receiveShip(10, 7, 'horizontal', battleship);
+  const endeavour = myModules.ship(4, 0, false, 5);
+  let result = myModules.gameboard.receiveShip(10, 7, 'horizontal', endeavour);
   expect(result).toBe("You can't place your ship there Admiral!");
 });
 
 test("gameboard should not be able to place a ship in an occupied space", () => {
-    myModules.gameboard.printBoard();
-    const destroyer = myModules.ship(3, 0, false)
+    const destroyer = myModules.ship(3, 0, false, 6)
     let result = myModules.gameboard.receiveShip(0, 0, 'horizontal', destroyer)
     expect(result).toBe("A ship already occupies this space Admiral!");
+})
+
+test("Gameboard receive attack function correctly registers a hit", () => {
+    let result = myModules.gameboard.receiveAttack(0, 0)
+    expect(myModules.ships[0].hitsTaken).toBe(1)
+    expect(result).toBe("Direct hit!")
+})
+
+test("Gameboard receive attack function correctly registers a miss", () => {
+  let result = myModules.gameboard.receiveAttack(3, 3);
+  expect(result).toBe("Miss!");
+  expect(myModules.gameboard.leBoard[3][3]).toBe('X');
+});
+
+test("Gameboard is able to register the fleet being destroyed", () => {
+    myModules.gameboard.receiveAttack(0, 1)
+    myModules.gameboard.receiveAttack(0, 2);
+    myModules.gameboard.receiveAttack(0, 3);
+    myModules.gameboard.receiveAttack(9, 9);
+    myModules.gameboard.receiveAttack(8, 9);
+    myModules.gameboard.receiveAttack(7, 9);
+    myModules.gameboard.receiveAttack(6, 9);
+    let result = myModules.gameboard.receiveAttack(5, 9);
+    expect(result).toBe("The fleet has been destroyed!");
+    myModules.gameboard.printBoard();
 })
