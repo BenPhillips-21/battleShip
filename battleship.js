0ec976f2ea6ce62ddb393e4120e693f9c6e89a45
HolidayGuy21 <14449@coderacademy.edu.au>
2023-12-29 23:03:47 +1100

    users can now add ships by clicking cell on gameboard

--- battleship.js
+++ battleship.js
@@ -1,7 +1,3 @@
-// let ships = [];
-
-import readline from "readline-sync";
-
 const ship = (length, hitsTaken, sunken, shipId) => ({
   length,
   hitsTaken,
@@ -72,6 +68,16 @@ const gameboard = {
         player.board[coordX][coordY] = ship.shipId;
         coordY += 1;
         t++;
+        console.log(t)
+        // t is the y coordinate
+        // the coordX coordinate stays the same
+
+        const cellId = `playerBoard-${coordX}-${t}`;
+        console.log(cellId)
+        const cell = document.getElementById(cellId);
+        if (cell) {
+          cell.classList.add('ship');
+        }
       }
     }
   },
@@ -111,9 +117,6 @@ checkFleet: function(playerFleet) {
     }
   },
 };
-
-console.log(gameboard.leBoard[1][0])
-console.log('le print')
 const createPlayer = (name, fleet) => {
   return {
     name,
@@ -122,7 +125,7 @@ const createPlayer = (name, fleet) => {
   };
 };
 
-const carrier1 = ship(5, 0, false, 5)
+const carrier1 = ship(5, 0, false, 5);
 const battleship1 = ship(4, 0, false, 4);
 const destroyer1 = ship(3, 0, false, 3);
 const submarine1 = ship(3, 0, false, 2);
@@ -140,89 +143,154 @@ const leFleet2 = [carrier2, battleship2, destroyer2, submarine2, patrolboat2];
 const player1 = createPlayer("Admiral Nelson", leFleet1);
 const player2 = createPlayer("Computer", leFleet2);
 
-// let result = gameboard.checkFleet(player1.fleet);
-// console.log(result)
-// gameboard.receiveShip(0, 0, "vertical", player1.fleet[0], player1)
-// gameboard.receiveAttack(0, 0, player1)
-
-gameboard.printBoard(player1)
+gameboard.printBoard(player1);
 gameboard.printBoard(player2);
 
-console.log(player1.fleet)
+console.log(player1.fleet);
 console.log(player2.fleet);
 
-console.log(player1.name)
-console.log(player2.name)
+console.log(player1.name);
+console.log(player2.name);
 
 let x = gameboard.checkFleet(player2.fleet);
 console.log(x);
 
-let shipNames = ["carrier", "battleship", "destroyer", "submarine", "patrolboat"];
+let shipNames = [
+  "carrier",
+  "battleship",
+  "destroyer",
+  "submarine",
+  "patrolboat",
+];
 
-for (var i = 0; i < player1.fleet.length; i++) {
-  gameboard.printBoard(player1);
-  console.log(`At what coordinates will you place your ${shipNames[i]} ${player1.name}?`);
-  let x = parseInt(readline.question(`At which X coordinate shall the ${shipNames[i]} be placed ${player1.name}? \n`));
-  let y = parseInt(readline.question(`At which Y coordinate shall the ${shipNames[i]} be placed ${player1.name}? \n`));
-  let direction = readline.question(`Shall the ship be placed vertically or horizontally ${player1.name}? horizontal / vertical \n`);
-  let result = gameboard.receiveShip(x, y, direction, player1.fleet[i], player1);
-    if (result === "You can't place your ship there Admiral!" || result === "A ship already occupies this space Admiral!") {
-        console.log(result)
-        i--
-      }
-  };
-for (var i = 0; i < player2.fleet.length; i++) {
-  let x = Math.floor(Math.random() * 9);
-  let y = Math.floor(Math.random() * 9);
-  let direction = Math.random() < 0.5 ? 'horizontal' : 'vertical';
-  let result = gameboard.receiveShip(x, y, direction, player2.fleet[i], player2)
-    if (result === "You can't place your ship there Admiral!" || result === "A ship already occupies this space Admiral!") {
-        i--;
+function generateBoard(boardId) {
+  const board = document.getElementById(boardId);
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.id = `${boardId}-${i}-${j}`;
+      cell.textContent = ""; // You can set initial values here if needed
+
+      // Add click event handler
+      cell.onclick = function () {
+        handleCellClick(cell.id);
+      };
+
+      board.appendChild(cell);
     }
+  }
 }
-gameboard.printBoard(player2)
+
+generateBoard("playerBoard")
+generateBoard("computerBoard")
+
+function handleCellClick() {
+  return new Promise(resolve => {
+    const board = document.getElementById("playerBoard");
+
+    // Create a click event listener
+    function clickHandler(event) {
+      const cellId = event.target.id;
+      console.log(cellId)
+      resolve(cellId);
+    }
+
+    // Add the click event listener to the board
+    board.addEventListener("click", clickHandler);
+  });
+}
+
+async function placeShips() {
+  for (var i = 0; i < player1.fleet.length; i++) {
+    var heading = document.getElementById("hiddenHeading");
+    heading.style.display = "block";
+    heading.textContent = `Select where you shall place the ${shipNames[i]} ${player1.name}`;
+    
+    // Console.log the prompt message
+    console.log(`Select where you shall place the ${shipNames[i]} ${player1.name}`);
+
+    let coords = await handleCellClick();
+
+    // Extract x and y from coords
+    let [x, y] = coords.split('-').slice(1).map(Number);
+
+    let direction = 'horizontal';
+
+    let result = gameboard.receiveShip(x, y, direction, player1.fleet[i], player1);
+    gameboard.printBoard(player1);
+    if (result === "You can't place your ship there Admiral!" || result === "A ship already occupies this space Admiral!") {
+      console.log(result);
+      i--;
+    }
+  }
+
+  for (var i = 0; i < player2.fleet.length; i++) {
+    let x = Math.floor(Math.random() * 9);
+    let y = Math.floor(Math.random() * 9);
+    let direction = Math.random() < 0.5 ? "horizontal" : "vertical";
+    let result = gameboard.receiveShip(x, y, direction, player2.fleet[i], player2);
+    if (result === "You can't place your ship there Admiral!" || result === "A ship already occupies this space Admiral!") {
+      i--;
+    }
+  }
+}
+
+async function gameLoop() {
+  await placeShips();
+  gameboard.printBoard(player2);
+}
+
+console.log("Le loop");
+gameLoop();
+
+
 
 console.log(`${player1.name}, it is time to attack!`);
 
 let isPlayer1Turn = true;
 
 const sendShot = () => {
-  let x = parseInt(readline.question(`At which X coordinate shall we send our shot ${player1.name}? \n`));
+  let x = parseInt(
+    readline.question(
+      `At which X coordinate shall we send our shot ${player1.name}? \n`
+    )
+  );
   let y = parseInt(readline.question(`And at which Y coordinate Admiral? \n`));
 
   if (isNaN(x) || isNaN(y)) {
     console.log("Please enter valid coordinates!");
-    return sendShot(); 
+    return sendShot();
   }
 
   if (x < 0 || x > 9 || y < 0 || y > 9) {
     console.log("Please enter coordinates within the valid range (0-9)!");
-    return sendShot(); 
+    return sendShot();
   }
 
   if (player2.board[x][y] === "M" || player2.board[x][y] === "H") {
     console.log("You've already fired a shot there Admiral! Let's try again...");
-    return sendShot(); 
+    return sendShot();
   }
-
   return [x, y];
 };
 
 const checkIfLegal = (x, y) => {
   if (!player1.board[x] || !player1.board[x][y]) {
-    return 'Illegal';
+    return "Illegal";
   }
   if (player1.board[x][y] === "M" || player1.board[x][y] === "H") {
-    return 'Illegal'
+    return "Illegal";
   }
   if (x > 9 || y > 9) {
-    return 'Illegal'
+    return "Illegal";
   }
-  return 'Legal'
-}
+  return "Legal";
+};
 
 const computerSendShot = () => {
-console.log(player1.board)
+  console.log(player1.board);
   for (let i = 0; i < player1.board.length; i++) {
     for (let j = 0; j < player1.board[i].length; j++) {
       if (player1.board[i][j] === "H") {
@@ -236,22 +304,22 @@ console.log(player1.board)
         d--;
 
         let r1 = checkIfLegal(a, j);
-        if (r1 === 'Legal') {
+        if (r1 === "Legal") {
           return [a, j];
         }
 
         let r2 = checkIfLegal(b, j);
-        if (r2 === 'Legal') {
+        if (r2 === "Legal") {
           return [b, j];
         }
 
         let r3 = checkIfLegal(i, c);
-        if (r3 === 'Legal') {
+        if (r3 === "Legal") {
           return [i, c];
         }
 
         let r4 = checkIfLegal(i, d);
-        if (r4 === 'Legal') {
+        if (r4 === "Legal") {
           return [i, d];
         }
         // if all moves are illegal, then do a random shot LOL
@@ -261,7 +329,7 @@ console.log(player1.board)
 
   let a = Math.floor(Math.random() * 9);
   let b = Math.floor(Math.random() * 9);
-  
+
   if (player1.board[a][b] === "M" || player1.board[a][b] === "H") {
     let result = computerSendShot();
     return result;
@@ -270,35 +338,39 @@ console.log(player1.board)
   return [a, b];
 };
 
-
-
-while (isPlayer1Turn) {
+function initializeGame() {
   gameboard.printBoard(player2);
-  let shotCoords = sendShot()
+  let shotCoords = sendShot();
   let result = gameboard.receiveAttack(shotCoords[0], shotCoords[1], player2);
-  console.log(result)
+  console.log(result);
   if (result === "The fleet has been destroyed!") {
-    console.log(`The enemy fleet has been destroyed ${player1.name}! Victory is ours!`)
-    break
+    console.log(
+      `The enemy fleet has been destroyed ${player1.name}! Victory is ours!`
+    );
+    return;
   }
-  let computerShotCoords = computerSendShot()
-  let outcome = gameboard.receiveAttack(computerShotCoords[0], computerShotCoords[1], player1);
-  gameboard.printBoard(player1)
-  console.log("^^^")
-    if (outcome === "Direct hit!") {
-      console.log("We've taken a hit Admiral!")
-    }
-    if (outcome === "Miss!") {
-      console.log("The enemy have missed us!")
-    }
-    if (outcome === "The fleet has been destroyed!") {
-      console.log("Our fleet has been destroyed!")
-      console.log(`We have been defeated ${player1.name}...`);
-      break
-    }
-} 
+  let computerShotCoords = computerSendShot();
+  let outcome = gameboard.receiveAttack(
+    computerShotCoords[0],
+    computerShotCoords[1],
+    player1
+  );
+  gameboard.printBoard(player1);
+  console.log("^^^");
+  if (outcome === "Direct hit!") {
+    console.log("We've taken a hit Admiral!");
+  }
+  if (outcome === "Miss!") {
+    console.log("The enemy have missed us!");
+  }
+  if (outcome === "The fleet has been destroyed!") {
+    console.log("Our fleet has been destroyed!");
+    console.log(`We have been defeated ${player1.name}...`);
+    return;
+  }
+}
 
+initializeGame();
 console.log("Game over");
-
 
 // module.exports = { ship, gameboard, ships };
